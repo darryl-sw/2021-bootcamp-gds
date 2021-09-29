dfc835596a94a4f85e6bc07bc534cfed5ce3fb0f
Darryl Sw <darryl_sw@hive.gov.sg>
2021-09-29 19:40:09 +0800

    Enable all CORS

--- backend/src/App.ts
+++ backend/src/App.ts
@@ -1,14 +1,14 @@
 import express from "express";
-
 import TodoRouter from "./routes";
 import swaggerUi from "swagger-ui-express";
 import swaggerJson from "../swagger.json";
+var cors = require('cors')
 
 const app = express();
 const port = process.env.PORT || 9000;
 
+app.use(cors())
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerJson));
-
 app.use(express.json());
 app.use("/api", TodoRouter);
 
